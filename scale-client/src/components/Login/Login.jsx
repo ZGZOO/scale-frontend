1502a1858aae153f66c9e8a7bcf7c83f38ea8368
Zhijie Xu <jennyxu000@gmail.com>
2020-08-12 10:34:42 -0700

    update log in page

--- scale-client/src/components/Login/Login.jsx
+++ scale-client/src/components/Login/Login.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handle submit -- ", event);
+  };
+
   return (
     <div className="login">
-      <NavLink to="/">&#8678; Back to Home</NavLink>
-      <br />
-      <br />
-      <form>
+      {/* <NavLink to="/">&#8678; Back to Home</NavLink> */}
+      <h3>Log In Page</h3>
+      <form onSubmit={handleSubmit}>
         <label>Username</label>
         <br />
         <input placeholder="Username"></input>
@@ -16,6 +20,12 @@ function Login() {
         <label>Password</label>
         <br />
         <input placeholder="Password"></input>
+        <br />
+        <br />
+        <button type="submit">Log In</button>
+        <NavLink to="/">
+          <button type="submit">Cancel</button>
+        </NavLink>
       </form>
     </div>
   );
