1dac20f9b30b595605b249a20c673de26bc97209
Zhijie Xu <jennyxu000@gmail.com>
2020-08-12 10:23:33 -0700

    login page layout

--- scale-client/src/components/Login/Login.jsx
+++ scale-client/src/components/Login/Login.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Login() {
   return (
     <div className="login">
+      <NavLink to="/">&#8678; Back to Home</NavLink>
+      <br />
+      <br />
       <form>
         <label>Username</label>
+        <br />
         <input placeholder="Username"></input>
+        <br />
+        <br />
         <label>Password</label>
+        <br />
         <input placeholder="Password"></input>
       </form>
     </div>
